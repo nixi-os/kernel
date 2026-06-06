5f9e5d67d7707b25bfac8f69b199c85f284ed4a4
Proxin <thugshaker627@gmail.com>
2026-06-06 09:36:12 +0200

    fix: refactor cpio implementation

--- src/kernel/parse/cpio.rs
+++ src/kernel/parse/cpio.rs
@@ -3,6 +3,26 @@
 use core::iter::Iterator;
 use core::slice::Iter;
 
+/// Size of a raw CPIO entry in bytes
+const RAW_ENTRY_SIZE: usize = core::mem::size_of::<RawEntry>();
+
+/// Raw byte representation of a CPIO entry
+#[repr(packed, C)]
+#[derive(Debug)]
+struct RawEntry {
+    signature: u16,
+    device: u16,
+    inode: u16,
+    mode: u16,
+    user_id: u16,
+    group_id: u16,
+    links: u16,
+    block_device: u16,
+    modified: [u8; 4],
+    path_size: u16,
+    data_size: [u8; 4],
+}
+
 /// A CPIO entry. The CPIO entry is stripped of all unused values
 #[derive(Debug)]
 pub struct CpioEntry<'a> {
@@ -21,17 +41,6 @@ impl<'a> CpioParser<'a> {
         CpioParser {
             bytes: bytes.iter(),
         }
-    }
-
-    /// Return the next chunk of N bytes and pad with (pad) bytes
-    fn next_chunk<const N: usize>(&mut self, pad: usize) -> Option<[u8; N]> {
-        let chunk = self.bytes.next_chunk::<N>().ok()?.map(|byte| *byte);
-
-        if pad > 0 {
-            self.bytes.nth(pad - 1);
-        }
-
-        Some(chunk)
     }
 
     /// Return the next n bytes and pad with (n % pad) bytes
@@ -58,17 +67,25 @@ impl<'a> Iterator for CpioParser<'a> {
     type Item = CpioEntry<'a>;
 
     fn next(&mut self) -> Option<CpioEntry<'a>> {
-        assert_eq!(u16::from_le_bytes(self.next_chunk::<2>(2)?), 0x71c7);
+        let bytes = self
+            .bytes
+            .next_chunk::<RAW_ENTRY_SIZE>()
+            .ok()?
+            .map(|byte| *byte);
 
-        let path_size = u16::from_le_bytes(self.next_chunk::<2>(0)?);
+        let entry = super::decode::<RawEntry>(&bytes);
 
-        let data_size_high = u16::from_le_bytes(self.next_chunk::<2>(0)?) as u32;
-        let data_size_low = u16::from_le_bytes(self.next_chunk::<2>(0)?) as u32;
-        let data_size = (data_size_high << 16) | data_size_low;
-
-        let path = str::from_utf8(self.next_bytes(path_size as usize - 1, 2)?).ok()?;
+        let path_bytes = self.next_bytes(entry.path_size as usize, 2)?;
+        let path = str::from_utf8(&path_bytes[..path_bytes.len() - 1]).ok()?;
 
         if path != "TRAILER!!!" {
+            let data_size = u32::from_le_bytes([
+                entry.data_size[2],
+                entry.data_size[3],
+                entry.data_size[0],
+                entry.data_size[1],
+            ]);
+
             Some(CpioEntry {
                 path,
                 data: self.next_bytes(data_size as usize, 2)?,
